9d356332497019fff8c39fb204f3056c8cb289a0
unknown <rehannajam2@gmail.com>
2024-02-14 19:48:14 +0500

    Chore: Integrated Clerk for smooth authentication

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,6 @@
+// IMPORTS -
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignIn />;
+}
---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,6 @@
+// IMPORTS -
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignUp />;
+}
