bcef6af6ae7caa024d8553d866779aae241e345a
unknown <rehannajam2@gmail.com>
2024-02-14 19:46:18 +0500

    Chore: Added a middleware and an env file

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+// IMPORTS -
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <div className="p-4">
+    <Button variant={"default"}>
+      Click here
+    </Button>
+  </div>
+}
