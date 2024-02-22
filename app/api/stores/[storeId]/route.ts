891bc250ee646a401b4248a2614b94d7a2b7f995
unknown <rehannajam2@gmail.com>
2024-02-22 23:22:50 +0500

    Chore: Created an API to update the store

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,43 @@
+// IMPORTS -
+import prismadb from "@/lib/prismaDB";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_PUT]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
